b5fd0b629027151747ff8302e381d9577beeba4d
pablo2389 <160980965+pablo2389@users.noreply.github.com>
2025-04-21 23:21:04 -0300

    Ignorar ESLint en build para Vercel

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-      ignoreDuringBuilds: true, // 👈 Esto desactiva que ESLint rompa el build
+      ignoreDuringBuilds: true,
     },
   }
   
